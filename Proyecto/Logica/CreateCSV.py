66783a6c9c87917e4b351d809f36e143b0a9df4b
GerardoVR7 <GvrZero_07@outlook.com>
2021-06-30 02:10:14 -0500

    avance1.0

--- Proyecto/Logica/CreateCSV.py
+++ Proyecto/Logica/CreateCSV.py
@@ -28,28 +28,64 @@ def generateChest():
     print('Cofre: ' , round(randomChest) )
 
     if round(randomChest) == 1:
+        typeChest = 'Plata'
         print('Plata')
     elif round(randomChest) == 2:
+        typeChest = 'Oro'
         print('Oro')
     elif round(randomChest) == 3:
+        typeChest = 'Gigante'
         print('Gigante')
     elif round(randomChest) == 4:
+        typeChest = 'Magico'
         print('Magico')
+    return typeChest
 
-def generateCarts():
+def generateCarts(typeChest):
     typeCard = ""
+    quantityCarts = ""
     randomCard = np.random.uniform(1,100)
 
     print('Carta: ' , round(randomCard) )
-    
-    if round(randomCard) >= 1 and round(randomCard) <= 58:
-        print('Comun')
-    elif round(randomCard) >= 59 and round(randomCard) <= 78:
-        print('Especiales')
-    elif round(randomCard) >= 79 and round(randomCard) <= 89:
-        print('Epicas')
-    elif round(randomCard) >= 90 and round(randomCard) <= 100:
-        print('Legendaria')
+
+    if (typeChest == 'Plata'):
+        if round(randomCard) >= 1 and round(randomCard) <= 69:
+            print('Comun')
+        elif round(randomCard) >= 70 and round(randomCard) <= 88:
+            print('Especiales')
+        elif round(randomCard) >= 89 and round(randomCard) <= 98:
+            print('Epicas')
+        elif round(randomCard) >= 99 and round(randomCard) <= 100:
+            print('Legendaria')
+    elif (typeChest == 'Oro'):
+        if round(randomCard) >= 1 and round(randomCard) <= 69:
+            print('Comun')
+        elif round(randomCard) >= 70 and round(randomCard) <= 85:
+            print('Especiales')
+        elif round(randomCard) >= 86 and round(randomCard) <= 96:
+            print('Epicas')
+        elif round(randomCard) >= 97 and round(randomCard) <= 100:
+            print('Legendaria')
+    elif (typeChest == 'Gigante'):
+        if round(randomCard) >= 1 and round(randomCard) <= 58:
+            print('Comun')
+        elif round(randomCard) >= 59 and round(randomCard) <= 78:
+            print('Especiales')
+        elif round(randomCard) >= 79 and round(randomCard) <= 92:
+            print('Epicas')
+        elif round(randomCard) >= 93 and round(randomCard) <= 100:
+            print('Legendaria')
+    elif (typeChest == 'Magico'):
+        if round(randomCard) >= 1 and round(randomCard) <= 58:
+            print('Comun')
+        elif round(randomCard) >= 59 and round(randomCard) <= 78:
+            print('Especiales')
+        elif round(randomCard) >= 79 and round(randomCard) <= 89:
+            print('Epicas')
+        elif round(randomCard) >= 90 and round(randomCard) <= 100:
+            print('Legendaria')
+
+    return typeCard, quantityCarts
 
 
 def calculateSamples(quantity):
