0f752dfe61275bf7d7529ef458cfca3ff74d4c04
GerardoVR7 <GvrZero_07@outlook.com>
2021-06-30 02:06:56 -0500

    avance

--- Proyecto/Main/main.py
+++ Proyecto/Main/main.py
@@ -1,10 +1,2 @@
-import pandas as pd
-import numpy as np
-
-def valoresCSV(ruta):
-
-    archivoCSV = pd.read_csv(ruta, header=0)
-    print(archivoCSV)
 
 
-valoresCSV("Archivos/simulacion_1k.csv")
