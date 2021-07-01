ec292c62f91f9c1b79fc486b7bbcb4a9a302deb2
GerardoVR7 <GvrZero_07@outlook.com>
2021-07-01 00:40:31 -0500

    avance 1.3 corrigiendo llenado de tabla

--- Proyecto/Logica/CreateCSV.py
+++ Proyecto/Logica/CreateCSV.py
@@ -65,72 +65,98 @@ def generateCarts(typeChest):
     typeCard = ""
     quantityCarts = ""
     randomCard = np.random.uniform(1,100)
-
     print('Carta: ' , round(randomCard) )
-
     if (typeChest == 'Plata'):
         if round(randomCard) >= 1 and round(randomCard) <= 69:
             print('Comun')
+            typeCard = 'Comun'
+            quantityCarts = 16
         elif round(randomCard) >= 70 and round(randomCard) <= 88:
-            print('Especiales')
+            print('Especial')
+            typeCard = 'Especial'
+            quantityCarts = 2
         elif round(randomCard) >= 89 and round(randomCard) <= 98:
-            print('Epicas')
+            print('Epica')
+            typeCard = 'Epica'
+            quantityCarts = 1
         elif round(randomCard) >= 99 and round(randomCard) <= 100:
             print('Legendaria')
+            typeCard = 'Legendaria'
+            quantityCarts = 1
     elif (typeChest == 'Oro'):
         if round(randomCard) >= 1 and round(randomCard) <= 69:
             print('Comun')
+            typeCard = 'Comun'
+            quantityCarts = 29
         elif round(randomCard) >= 70 and round(randomCard) <= 85:
-            print('Especiales')
+            print('Especial')
+            typeCard = 'Especial'
+            quantityCarts = 10
         elif round(randomCard) >= 86 and round(randomCard) <= 96:
-            print('Epicas')
+            print('Epica')
+            typeCard = 'Epica'
+            quantityCarts = 3
         elif round(randomCard) >= 97 and round(randomCard) <= 100:
             print('Legendaria')
+            typeCard = 'Legendaria'
+            quantityCarts = 1
     elif (typeChest == 'Gigante'):
         if round(randomCard) >= 1 and round(randomCard) <= 58:
             print('Comun')
+            typeCard = 'Comun'
+            quantityCarts = 300
         elif round(randomCard) >= 59 and round(randomCard) <= 78:
-            print('Especiales')
+            print('Especial')
+            typeCard = 'Especial'
+            quantityCarts = 74
         elif round(randomCard) >= 79 and round(randomCard) <= 92:
             print('Epicas')
+            typeCard = 'Epicas'
+            quantityCarts = 6
         elif round(randomCard) >= 93 and round(randomCard) <= 100:
             print('Legendaria')
+            typeCard = 'Legendaria'
+            quantityCarts = 1
     elif (typeChest == 'Magico'):
         if round(randomCard) >= 1 and round(randomCard) <= 58:
             print('Comun')
+            typeCard = 'Comun'
+            quantityCarts = 72
         elif round(randomCard) >= 59 and round(randomCard) <= 78:
-            print('Especiales')
+            print('Especial')
+            typeCard = 'Especial'
+            quantityCarts = 21
         elif round(randomCard) >= 79 and round(randomCard) <= 89:
-            print('Epicas')
+            print('Epica')
+            typeCard = 'Epica'
+            quantityCarts = 10
         elif round(randomCard) >= 90 and round(randomCard) <= 100:
             print('Legendaria')
+            typeCard = 'Legendaria'
+            quantityCarts = 1
     elif (typeChest == 'SuperMagico'):
-        if round(randomCard) >= 1 and round(randomCard) <= 40:
-            print('Comun')
-        elif round(randomCard) >= 41 and round(randomCard) <= 69:
-            print('Especiales')
-        elif round(randomCard) >= 70 and round(randomCard) <= 88:
-            print('Epicas')
+        if round(randomCard) >= 1 and round(randomCard) <= 88:
+            print('Epica')
+            typeCard = 'Epica'
+            quantityCarts = 35
         elif round(randomCard) >= 89 and round(randomCard) <= 100:
             print('Legendaria')
-    elif (typeChest == 'Cofre del Rey'):
-        if round(randomCard) >= 1 and round(randomCard) <= 38:
-            print('Comun')
-        elif round(randomCard) >= 39 and round(randomCard) <= 59:
-            print('Especiales')
-        elif round(randomCard) >= 60 and round(randomCard) <= 85:
-            print('Epicas')
-        elif round(randomCard) >= 86 and round(randomCard) <= 100:
-            print('Legendaria')
-    elif (typeChest == 'Cofre del Rey Legendario'):
+            typeCard = 'Legendaria'
+            quantityCarts = 1
+    elif (typeChest == 'SuperEspecial'):
         if round(randomCard) >= 1 and round(randomCard) <= 40:
             print('Comun')
-        elif round(randomCard) >= 41 and round(randomCard) <= 68:
-            print('Especiales')
-        elif round(randomCard) >= 69 and round(randomCard) <= 79:
-            print('Epicas')
-        elif round(randomCard) >= 80 and round(randomCard) <= 100:
-            print('Legendaria')        
+            typeCard = 'Comun'
+            quantityCarts = 400
+        elif round(randomCard) >= 41 and round(randomCard) <= 100:
+            print('Especial')
+            typeCard = 'Especial'
+            quantityCarts = 120
+    elif (typeChest == 'Legendario'):
+        if round(randomCard) >= 1 and round(randomCard) <= 100:
+            print('Legendaria')  
+            typeCard = 'Legendaria'
+            quantityCarts = 1      
 
     return typeCard, quantityCarts
 
