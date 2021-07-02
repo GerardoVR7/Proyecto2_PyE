36de9b2d04714006a5a1c959ba8354acd0d0e3d5
GerardoVR7 <GvrZero_07@outlook.com>
2021-07-02 13:24:19 -0500

    avance2.0

--- Proyecto/Logica/CreateCSV.py
+++ Proyecto/Logica/CreateCSV.py
@@ -3,8 +3,8 @@ import numpy as np
 import math
 
 global superPlay
-superPlay = None
-superPlay = 240
+superPlay = False
+
 global rutaArchivo
 rutaArchivo = None
 rutaArchivo = "Proyecto/Archivos/simulacion_1k.csv"
@@ -13,7 +13,7 @@ def valoresCSV(ruta):
     archivoCSV = pd.read_csv(ruta, header=0)
     
     print(archivoCSV)
-    totalPlays, totalChest, totalCarts, totalQuantity = calculateSamples(100)
+    totalPlays, totalChest, totalCarts, totalQuantity = calculateSamples(1000)
     archivoCSV ["Plays"] = totalPlays
     archivoCSV ["Chest"] = totalChest
     archivoCSV ["Carts"] = totalCarts
@@ -23,143 +23,136 @@ def valoresCSV(ruta):
 def generateWins():
     thePlay = ""
     randomWin = np.random.uniform(1,2)
-    print('Numpy: ' , round(randomWin) )
-    print('Numpy: ' , randomWin )
     if ( round(randomWin) == 1):
         thePlay = "win"
     else:
-        thePlay = "lose"
-    
-    print(thePlay)
+        thePlay = "lose"    
     return thePlay
 
-def generateChest(play):
-    typeChest = ""
-    print("Super play" , superPlay)
-    if (play == superPlay):
+def generateChest(play, especialCase):
+    typeChest = ""    
+    print(play)
+    print(especialCase)
+    if (play == especialCase):
         randomChest = np.random.uniform(1,3)
         if round(randomChest) == 1:
             typeChest = 'SuperMagico'
-            print('SuperMagico')
+
         elif round(randomChest) == 2:
             typeChest = 'SuperEspecial'
-            print('SuperEspecial')
+
         elif round(randomChest) == 3:
             typeChest = 'Legendario'
-            print('Legendario')
-        
-        superPlay + 240
-        
     else:
         randomChest = np.random.uniform(1,4)
         if round(randomChest) == 1:
             typeChest = 'Plata'
-            print('Plata')
+
         elif round(randomChest) == 2:
             typeChest = 'Oro'
-            print('Oro')
+
         elif round(randomChest) == 3:
             typeChest = 'Gigante'
-            print('Gigante')
+
         elif round(randomChest) == 4:
             typeChest = 'Magico'
-            print('Magico')
+
     return typeChest
 
 def generateCarts(typeChest):
     typeCard = ""
     quantityCarts = ""
     randomCard = np.random.uniform(1,100)
-    print('Carta: ' , round(randomCard) )
+
     if (typeChest == 'Plata'):
         if round(randomCard) >= 1 and round(randomCard) <= 69:
-            print('Comun')
+
             typeCard = 'Comun'
             quantityCarts = 16
         elif round(randomCard) >= 70 and round(randomCard) <= 88:
-            print('Especial')
+
             typeCard = 'Especial'
             quantityCarts = 2
         elif round(randomCard) >= 89 and round(randomCard) <= 98:
-            print('Epica')
+
             typeCard = 'Epica'
             quantityCarts = 1
         elif round(randomCard) >= 99 and round(randomCard) <= 100:
-            print('Legendaria')
+
             typeCard = 'Legendaria'
             quantityCarts = 1
     elif (typeChest == 'Oro'):
         if round(randomCard) >= 1 and round(randomCard) <= 69:
-            print('Comun')
+
             typeCard = 'Comun'
             quantityCarts = 29
         elif round(randomCard) >= 70 and round(randomCard) <= 85:
-            print('Especial')
+
             typeCard = 'Especial'
             quantityCarts = 10
         elif round(randomCard) >= 86 and round(randomCard) <= 96:
-            print('Epica')
+
             typeCard = 'Epica'
             quantityCarts = 3
         elif round(randomCard) >= 97 and round(randomCard) <= 100:
-            print('Legendaria')
+
             typeCard = 'Legendaria'
             quantityCarts = 1
     elif (typeChest == 'Gigante'):
         if round(randomCard) >= 1 and round(randomCard) <= 58:
-            print('Comun')
+
             typeCard = 'Comun'
             quantityCarts = 300
         elif round(randomCard) >= 59 and round(randomCard) <= 78:
-            print('Especial')
+
             typeCard = 'Especial'
             quantityCarts = 74
         elif round(randomCard) >= 79 and round(randomCard) <= 92:
-            print('Epicas')
+
             typeCard = 'Epicas'
             quantityCarts = 6
         elif round(randomCard) >= 93 and round(randomCard) <= 100:
-            print('Legendaria')
+
             typeCard = 'Legendaria'
             quantityCarts = 1
     elif (typeChest == 'Magico'):
         if round(randomCard) >= 1 and round(randomCard) <= 58:
-            print('Comun')
+
             typeCard = 'Comun'
             quantityCarts = 72
         elif round(randomCard) >= 59 and round(randomCard) <= 78:
-            print('Especial')
+
             typeCard = 'Especial'
             quantityCarts = 21
         elif round(randomCard) >= 79 and round(randomCard) <= 89:
-            print('Epica')
+
             typeCard = 'Epica'
             quantityCarts = 10
         elif round(randomCard) >= 90 and round(randomCard) <= 100:
-            print('Legendaria')
+
             typeCard = 'Legendaria'
             quantityCarts = 1
     elif (typeChest == 'SuperMagico'):
         if round(randomCard) >= 1 and round(randomCard) <= 88:
-            print('Epica')
+
             typeCard = 'Epica'
             quantityCarts = 35
         elif round(randomCard) >= 89 and round(randomCard) <= 100:
-            print('Legendaria')
+
             typeCard = 'Legendaria'
             quantityCarts = 1
     elif (typeChest == 'SuperEspecial'):
         if round(randomCard) >= 1 and round(randomCard) <= 40:
-            print('Comun')
+
             typeCard = 'Comun'
             quantityCarts = 400
         elif round(randomCard) >= 41 and round(randomCard) <= 100:
-            print('Especial')
+
             typeCard = 'Especial'
             quantityCarts = 120
     elif (typeChest == 'Legendario'):
         if round(randomCard) >= 1 and round(randomCard) <= 100:
-            print('Legendaria')  
+
             typeCard = 'Legendaria'
             quantityCarts = 1      
 
@@ -171,16 +164,19 @@ def calculateSamples(quantity):
     chest = []
     carts = []
     quantityList = []
+    especialCase = 241
+    wins = 0
     for i in range (quantity):
         gameResult = generateWins()
-        print(gameResult)
         plays.append(gameResult)
         if (gameResult =="win"):
-            chestResult = generateChest(i)
+            wins = wins + 1
+            chestResult = generateChest(wins,especialCase)
             chest.append(chestResult)
             cartResult, quantityResult = generateCarts(chestResult)
             carts.append(cartResult)
             quantityList.append(quantityResult)
+            
         else: 
             chest.append("---")
             carts.append("---")
--- Proyecto/Logica/ProbabilidadClasica.py
+++ Proyecto/Logica/ProbabilidadClasica.py
@@ -1,31 +1,84 @@
-from CreateCSV import rutaArchivo
+from CreateCSV import rutaArchivo, valoresCSV
 
 import pandas as pd
 import numpy as np
 
 
-def arrayCreation(self):
+def arrayCreation():
        global play
        global arrayCofre
        global arrayCarta
        global arrayQuantity 
-       
+       valoresCSV(rutaArchivo)
        df= pd.read_csv(rutaArchivo)
        play = np.array(df['Plays'].sort_values())
        arrayCofre = np.array(df['Chest'].sort_values())
        arrayCarta = np.array(df['Carts'].sort_values())
        arrayQuantity = np.array(df['Quantity'].sort_values())
+       len(play)
 
-
-def clasicProbability():
+def Probability():
        winCases = 0
+       loseCases = 0
+       plataChestCases = 0
+       oroChestCases = 0
+       giganteChestCases = 0
+       magicoChestCases = 0
+       superMagicoChestCases = 0
+       superEspecialChestCases = 0
+       legendarioChestCases = 0
+
        for i in range(len(play)):
 
               if (play[i] == "win"):
                      winCases = winCases + 1
+                     if (arrayCofre[i] == "Plata"):
+                            plataChestCases = plataChestCases + 1
+                     if (arrayCofre[i] == "Oro"):
+                            oroChestCases = oroChestCases + 1
+                     if (arrayCofre[i] == "Gigante"):
+                            giganteChestCases = giganteChestCases + 1
+                     if (arrayCofre[i] == "Magico"):
+                            magicoChestCases = magicoChestCases + 1
+                     if (arrayCofre[i] == "SuperMagico"):
+                            superMagicoChestCases = superMagicoChestCases + 1
+                     if (arrayCofre[i] == "SuperEspecial"):
+                            superEspecialChestCases = superEspecialChestCases + 1
+                     if (arrayCofre[i] == "Legendario"):
+                            legendarioChestCases = legendarioChestCases + 1
+              else:
+                     loseCases = loseCases + 1
 
-       winProbability = 
+       winProbability = winCases / len(play)
+       loseProbability = loseCases / len(play)
+       plataProbability = plataChestCases / len(play)
+       oroProbability = oroChestCases / len(play)
+       giganteProbability = giganteChestCases / len(play)
+       magicoProbability = magicoChestCases / len(play)
+       superMagicoProbability = superMagicoChestCases / len(play)
+       superEspecialProbability = superEspecialChestCases / len(play)
+       legendarioProbability =  legendarioChestCases / len(play)
+       print('Casos ganados: ' , winCases)
+       print(winProbability)
+       print(loseProbability)
 
+       print('Casos plata: ' , plataChestCases)
+       print(plataProbability)
+       print('Casos Oro: ' , oroChestCases)
+       print(oroProbability)
+       print('Casos Gigante: ' , giganteChestCases)
+       print(giganteProbability)
+       print('Casos Magicos: ' , magicoChestCases)
+       print(magicoProbability)
+       print('Casos Super Magico: ' , superMagicoChestCases)
+       print(superMagicoProbability)
+       print('Casos Super especial: ' , superMagicoChestCases)
+       print(superEspecialProbability)
+       print('Casos Legendarios: ' , legendarioChestCases)
+       print(legendarioProbability)
+
+arrayCreation()
+Probability()
 
 
 
